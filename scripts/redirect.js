60cd2b7aacb137fb1839b28bb36becb11177bf2a
MSWS <imodmaker@gmail.com>
2022-11-23 00:59:48 -0800

    Add 404 redirects

---
+++ scripts/redirect.js
@@ -0,0 +1,18 @@
+let target = window.location.href;
+let matches = RegExp("(:|.)([0-9]|\S){2,5}\/.+").exec(target);
+
+let match = target.substring(matches.index, matches.index + matches[0].length);
+match = match.substring(match.indexOf("/") + 1);
+console.log("Match: " + match);
+
+const map = {
+  "github": "https://github.com/MSWS/",
+  "youtube": "https://www.youtube.com/@MSWS/",
+  "jbmaps": "https://docs.google.com/spreadsheets/d/1V4IP8lJe1Y7VmWusPT4166AlRQX3Tj-BSG7J_V3cGL4/edit?usp=sharing",
+  "sonitor": "https://www.edgegamers.com/threads/374889/",
+  "playlist": "https://music.youtube.com/playlist?list=PLgCdlfyStH3fMphQSEQYMB1N0RM7h8xBU",
+  "discord": "https://discord.gg/A4dFdzZFHh"
+}
+
+if (match in map)
+  window.location.href = map[match];
