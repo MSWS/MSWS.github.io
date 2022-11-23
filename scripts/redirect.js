1c623995250714910042a8a9d72b80f20e48dbc9
MSWS <imodmaker@gmail.com>
2022-11-23 01:27:29 -0800

    Update case sensitivity and resume

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -2,7 +2,7 @@ let target = window.location.href;
 let matches = RegExp('(:|\\.)([0-9]|\\S){2,5}\\/.+').exec(target);
 
 let match = target.substring(matches.index, matches.index + matches[0].length);
-match = match.substring(match.indexOf("/") + 1);
+match = match.substring(match.indexOf("/") + 1).toLowerCase();
 
 const map = {
   "github": "https://github.com/MSWS/",
@@ -10,7 +10,8 @@ const map = {
   "jbmaps": "https://docs.google.com/spreadsheets/d/1V4IP8lJe1Y7VmWusPT4166AlRQX3Tj-BSG7J_V3cGL4/edit?usp=sharing",
   "sonitor": "https://www.edgegamers.com/threads/374889/",
   "playlist": "https://music.youtube.com/playlist?list=PLgCdlfyStH3fMphQSEQYMB1N0RM7h8xBU",
-  "discord": "https://discord.gg/A4dFdzZFHh"
+  "discord": "https://discord.gg/A4dFdzZFHh",
+  "resume": "https://msws.xyz/resources/resume.pdf"
 }
 
 if (match in map)
