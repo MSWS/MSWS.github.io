3ee5b13fea26ff2c8dd4933a7601338cd2832245
MSWS <imodmaker@gmail.com>
2022-11-23 00:31:58 -0800

    Update copyright automatically

--- scripts/footer.js
+++ scripts/footer.js
@@ -7,7 +7,14 @@ if (footer == null) {
 
 let text = document.createElement('p');
 text.classList.add('footer');
-text.appendChild(document.createTextNode("©2022 Isaac Boaz. All Rights Reserved."));
+
+let years = "2022";
+let year = new Date().getFullYear();
+if (year + "" != years) {
+  years += "-" + year % 100;
+}
+
+text.appendChild(document.createTextNode("©" + years + " Isaac Boaz. All Rights Reserved."));
 text.appendChild(document.createElement('br'));
 text.appendChild(document.createTextNode("Last Updated: " + document.lastModified));
 footer.appendChild(text)
