17fc84c4e390a74282f0fc31ddf1973cba5e3430
MSWS <imodmaker@gmail.com>
2023-10-30 11:40:47 -0700

    Start work on homelab

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,35 +1,40 @@
 const pages = {
-  "Home": "index.html",
-  "About": "about.html",
-  "Discord": "https://msws.xyz/discord",
-  "Paste": "https://paste.msws.xyz/",
-  "Stats": "https://msws.xyz/stats"
+    "Home": "index.html",
+    "About": "about.html",
+    "Homelab": "homelab.html",
+    "|": null,
+    "Discord": "https://msws.xyz/discord",
+    "Paste": "https://paste.msws.xyz/",
+    "Stats": "https://msws.xyz/stats"
 }
 
 window.addEventListener("load", init);
 
 function init() {
-  let header = document.querySelector('header');
-  if (header == null) {
-    header = document.createElement('header');
-    document.body.before(header);
-  }
+    let header = document.querySelector('header');
+    if (header == null) {
+        header = document.createElement('header');
+        document.body.before(header);
+    }
 
-  let ul = document.createElement('ul');
-  ul.classList.add('header');
+    let ul = document.createElement('ul');
+    ul.classList.add('header');
 
-  for (let page in pages) {
-    let a = document.createElement("a");
-    a.appendChild(document.createTextNode(page));
+    const path = window.location.pathname.split("/").pop();
 
-    a.href = pages[page];
+    for (let page in pages) {
+        let a = document.createElement("a");
+        a.appendChild(document.createTextNode(page));
 
-    let path = window.location.pathname.split("/").pop();
-    if (pages[page] == path || path == "" && pages[page] == "index.html")
-      a.classList.add("current");
-    ul.append(a);
-  }
+        if (pages[page])
+            a.href = pages[page];
 
-  header.prepend(ul);
+        if (pages[page] == path || path == "" && pages[page] == "index.html")
+            a.classList.add("current");
+
+        ul.append(a);
+    }
+
+    header.prepend(ul);
 }
 
