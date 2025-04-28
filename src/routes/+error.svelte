19d6fb1c1eb53f70e0199ae29d17bc62d0c42b4c
MSWS <imodmaker@gmail.com>
2025-04-28 01:19:05 -0700

    feat: Add custom error page and configure 404 fallback
    - Add a new Svelte error page component with error details and TailwindCSS styling
    - Update Svelte adapter fallback option to '404.html' in configuration
    
    [src/routes/+error.svelte]
    Created a new Svelte error page component that displays error information, including status and message, styled with flexbox and TailwindCSS classes.
    [svelte.config.js]
    - Changed the fallback option from undefined to '404.html' in the adapter configuration.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<div class="flex w-screen h-screen bg-grey-300 items-center justify-center">
+  <div class="flex-col text-center">
+    <p class="text-4xl font-bolder text-red-500">Error</p>
+    <span class="font-bold text-red-900">{page.status.toString()}</span>
+    <span>{page.error?.message}</span>
+  </div>
+</div>
