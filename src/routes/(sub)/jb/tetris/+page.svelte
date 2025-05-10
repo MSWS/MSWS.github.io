e1f6020f941109b589f1221e233d8a3cdcfad7f8
MSWS <imodmaker@gmail.com>
2025-05-10 00:11:33 -0700

    feat: Revamp Tetris layout and add game entry component
    - Update Tetris page layout with improved styling, dynamic badges, and a grid system for game entries
    - Add new `tetrisGameEntry.svelte` component to display game names with optional emotes, utilizing TypeScript

--- src/routes/(sub)/jb/tetris/+page.svelte
+++ src/routes/(sub)/jb/tetris/+page.svelte
@@ -4,13 +4,22 @@
   import Navbar from "../../../navbar.svelte";
 
   import TetrisJPG from "$lib/assets/tetris/tetris.jpg";
+  import FirstGame from "../badges/firstGame.svelte";
+  import SupportsTeams from "../badges/supportsTeams.svelte";
+  import RequiresHp from "../badges/requiresHp.svelte";
+  import KillsPlayers from "../badges/killsPlayers.svelte";
+  import TetrisGameEntry from "./tetrisGameEntry.svelte";
 </script>
 
-<div class="p-3">
+<div class="p-3 font-pixel text-2xl">
   <div class="ml-2 md:ml-0">
     <Navbar>
       {#snippet header()}
-        <img src={TetrisSVG} class="w-full ml-2 mr-2 md:mr-4" alt="jb_tetris" />
+        <img
+          src={TetrisSVG}
+          class="w-fit ml-auto bg-cyan-200/80 p-1 rounded-md"
+          alt="jb_tetris"
+        />
       {/snippet}
     </Navbar>
   </div>
@@ -21,22 +30,142 @@
     alt="jb_tetris"
   />
 
-  <div class="font-pixel text-2xl">
-    Enter the world of <em
+  <div class="bg-cyan-200/80 rounded-lg p-2 text-justify md:tracking-wide">
+    Enter the world of
+    <em
       ><a
         href="https://steamcommunity.com/sharedfiles/filedetails/?id=3467258615"
         class="text-blue-900 hover:text-blue-500">jb_tetris</a
       ></em
     >, a map inspired by the classic block-placing game. While this map may have
     a high skill floor, it features many challenging secrets, minigames, and a
-    healthy balance for both teams.
+    healthy balance for <span title="because there are two">both teams</span>.
   </div>
 
-  <div class="font-pixel text-2xl">
-    <img
-      src={GamesSVG}
-      class="bg-black/10 w-full mr-2 md:mr-4 h-20"
-      alt="jb_tetris"
-    />
+  <img src={GamesSVG} class="h-20 ml-auto m-2" alt="Games" />
+
+  <div
+    class="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-flow-row bg-cyan-900/20 p-2 divide-zinc-600 divide-x-8"
+  >
+    <TetrisGameEntry name="HNS">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+        <RequiresHp></RequiresHp>
+        <SupportsTeams></SupportsTeams>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Surf">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Race">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="React">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="BHop">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Maze">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Climb">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Colors">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+        <KillsPlayers></KillsPlayers>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Jumps">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Rhythm">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Territory">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+        <SupportsTeams></SupportsTeams>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Dodge It">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+        <RequiresHp></RequiresHp>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Gun Toss">
+      {#snippet emotes()}{/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="4 Corners">
+      {#snippet emotes()}{/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Dropdown">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Countdown">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Invis Climb">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Jump Rope">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+        <RequiresHp></RequiresHp>
+      {/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Long Jump">
+      {#snippet emotes()}{/snippet}
+    </TetrisGameEntry>
+
+    <TetrisGameEntry name="Dodge Course">
+      {#snippet emotes()}
+        <FirstGame></FirstGame>
+        <RequiresHp></RequiresHp>
+      {/snippet}
+    </TetrisGameEntry>
   </div>
 </div>
---
+++ src/routes/(sub)/jb/tetris/tetrisGameEntry.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let { name, emotes = undefined } = $props();
+</script>
+
+<div class="flex flex-row w-full items-center">
+  {#if emotes}
+    <div class="w-1/2 text-center">{name}</div>
+    <div class="w-1/2">
+      {@render emotes?.()}
+    </div>
+  {:else}
+    <div class="text-center w-full">{name}</div>
+  {/if}
+</div>
